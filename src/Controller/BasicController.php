ae661cdea7c3b8f59f4c068d8d1391b6ff4ddee6
Szymon Sadło <szymon.sadlo@qpony.pl>
2020-12-10 16:50:28 +0100

    Basic controller

---
+++ src/Controller/BasicController.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class BasicController extends AbstractController
+{
+    /**
+     * @Route("/basic", name="basic")
+     */
+    public function basic(): Response
+    {
+        $number = random_int(0, 100);
+
+        return new JsonResponse([
+            'number' => $number
+        ]);
+    }
+}
